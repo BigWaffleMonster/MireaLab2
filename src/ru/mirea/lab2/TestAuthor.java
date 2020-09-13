3607410acaca6025ce35ae60fa23e0d95f2f51ba
BigWaffleMonster <rachkov.daniil2001@gmail.com>
2020-09-13 12:19:06 +0300

    new files

---
+++ src/ru/mirea/lab2/TestAuthor.java
@@ -0,0 +1,9 @@
+package ru.mirea.lab2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("John", "John@mail.ru", 'm');
+
+        System.out.println(a1);
+    }
+}
